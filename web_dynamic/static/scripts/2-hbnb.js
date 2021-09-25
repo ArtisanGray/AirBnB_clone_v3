2567a3fbd21474deb4b94f537618b7b29cb5df4c
root <gnnrbachelor@gmail.com>
2021-09-25 20:33:33 +0000

    Fix task 3

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,11 +1,21 @@
-  $.get('http://127.0.0.1:5001/api/v1/status/', function (data, stat) {
-    if (stat === 'success') {
-      console.log(data);
-      if (data.status === 'OK') {
-       console.log("Good");
-      }
-      else {
-      console.log("Bullshit");
-     }
+$(() => {
+
+  const url = 'http://' + window.location.hostname + ':5001/api/v1/status/';
+  $.get(url, function (response) {
+    if (response.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
     }
+  });
+
+  const Amenities = {};
+  $('div.amenities li input').change( function () {
+      if ($(this).is(':checked')) {
+        Amenities[($(this).attr('data-id'))] = $(this).attr('data-name');
+      } else {
+        delete Amenities[($(this).attr('data-id'))];
+      }
+      $('div.amenities h4').html(Object.values(Amenities).join(', ') || '&nbsp;');
+    });
 });
